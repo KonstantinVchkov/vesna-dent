63fa68ec05fed7c44f0e56c97b85af0dc37ab703
Konstantin <kvckov@yahoo.com>
2024-01-29 17:40:17 +0100

    components created, styles created, dependencies installed

---
+++ vesna-dent/src/components/Banner/BannerSection.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from './styles.css'
+const BannerSection = () => {
+  return (
+    <h1 className={styles.Banner}>
+      Hello world!
+    </h1>
+  );
+};
+
+export default BannerSection;
