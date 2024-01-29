8adf7849fc4403d2f5a0aa4743300dd0cd8bc2dd
Konstantin <kvckov@yahoo.com>
2024-01-29 11:22:22 +0100

    components created, types folder created, tailwind dependencies installed

---
+++ Development/src/components/Banner/BannerSection.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BannerSection = () => {
+  return (
+    <h1 className="Banner-Section">
+      Hello world!
+    </h1>
+  );
+};
+
+export default BannerSection;
