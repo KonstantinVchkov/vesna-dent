bd8dad812a9101b4a28ae5f06b88091cc4c45fc5
Konstantin <kvckov@yahoo.com>
2024-02-02 19:52:37 +0100

    service cards done, created one more MainServiceComponent, also framer motion added to the cards

--- src/components/InfoSection/Services/ServiceOffer.tsx
+++ src/components/InfoSection/Services/ServiceOffer.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import Link from "next/link";
+import styles from "./styles.module.scss";
+import { motion } from "framer-motion";
+export type TOfferCard = {
+  id?: number;
+  title: string;
+  img: string;
+  paragraph: string;
+};
+const ServiceOfferCard = ({ title, img, paragraph }: TOfferCard) => (
+  <motion.div
+    initial={{ opacity: 0, y: -20 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ duration: 1 }}
+    className={styles.article}
+  >
+    <figure className={styles.figure}>
+      <img src={img} alt="dental-img.png" />
+    </figure>
+    <div className={styles.articleBody}>
+      <h2>{title}</h2>
+      <p>{paragraph}</p>
+      <Link href={""} className={styles.readMore}>
+        Прочитајте повеќе
+      </Link>
+    </div>
+  </motion.div>
+);
 
-const ServiceOffer = () => {
-  return (
-    <div>ServiceOffer</div>
-  )
-}
-
-export default ServiceOffer
+export default ServiceOfferCard;
