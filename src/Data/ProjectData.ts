67b5e4f245ea88a7227bbd3026aaea3d41c84d65
Konstantin Vckov <kvckov@yahoo.com>
2024-02-03 14:22:09 +0100

    count up done, only left the styles to be added

--- src/Data/ProjectData.ts
+++ src/Data/ProjectData.ts
@@ -3,7 +3,7 @@ export const NavList = [
   { title: "За нас", path: "about" },
   { title: "Услуги", path: "services" },
   { title: "Наша Работа", path: "ourwork" },
-  { title: "Стоматолози", path: "dentists" },
+  // { title: "Стоматолози", path: "dentists" },
 ];
 export const MainText = {
   title: "НАСМЕВКАТА Е НАЈВАЖНА",
@@ -20,39 +20,59 @@ export const OurWork = [];
 
 export const ServicesOffer = [
   {
-    id:1,
+    id: 1,
     img: "https://images.unsplash.com/photo-1600170311833-c2cf5280ce49?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     title: "Prosthodontics Dental Implants",
-    paragraph: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti mollitia nulla, tempore odit natus maxime.",
+    paragraph:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti mollitia nulla, tempore odit natus maxime.",
   },
   {
-    id:2,
+    id: 2,
     img: "https://images.unsplash.com/photo-1600170311833-c2cf5280ce49?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     title: "Oral & Maxillofacial Surgery",
-    paragraph: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti mollitia nulla, tempore odit natus maxime.",
+    paragraph:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti mollitia nulla, tempore odit natus maxime.",
   },
   {
-    id:3,
+    id: 3,
     img: "https://images.unsplash.com/photo-1600170311833-c2cf5280ce49?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     title: "Orthodontics and The Braces",
-    paragraph: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti mollitia nulla, tempore odit natus maxime.",
+    paragraph:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti mollitia nulla, tempore odit natus maxime.",
   },
   {
-    id:4,
+    id: 4,
     img: "https://images.unsplash.com/photo-1600170311833-c2cf5280ce49?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     title: "Endodontics",
-    paragraph: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti mollitia nulla, tempore odit natus maxime.",
+    paragraph:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti mollitia nulla, tempore odit natus maxime.",
   },
   {
-    id:5,
+    id: 5,
     img: "https://images.unsplash.com/photo-1600170311833-c2cf5280ce49?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     title: "Minimum Intervention Dentistry",
-    paragraph: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti mollitia nulla, tempore odit natus maxime.",
+    paragraph:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti mollitia nulla, tempore odit natus maxime.",
   },
   {
-    id:6,
+    id: 6,
     img: "https://images.unsplash.com/photo-1600170311833-c2cf5280ce49?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     title: "Pediatric Dentistry",
-    paragraph: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti mollitia nulla, tempore odit natus maxime.",
+    paragraph:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti mollitia nulla, tempore odit natus maxime.",
+  },
+];
+
+export const ourMilestones = [
+  {
+    id: 1,
+    title: "Задоволни клиенти",
+    stones: 2500,
+  },
+  { id: 2, title: "Награди", stones: 5 },
+  {
+    id: 3,
+    title: "Извршени интервенции",
+    stones: 3000,
   },
 ];
