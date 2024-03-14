0128f1650fa13329e8fced65437154eb8be3971b
Konstantin Vckov <kvckov@yahoo.com>
2024-03-14 23:42:57 +0100

    initial commit

--- src/components/InfoSection/Services/MainServiceComp.tsx
+++ src/components/InfoSection/Services/MainServiceComp.tsx
@@ -1,21 +1,43 @@
-"use-client";
-import { ServicesOffer } from "@/Data/ProjectData";
 import React from "react";
-import ServiceOfferCard from "./ServiceOffer";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 import styles from "./styles.module.scss";
+
+// Import Swiper styles
+import "swiper/css";
+
+// Import required modules
+import { Navigation, Pagination, Scrollbar, A11y, Mousewheel } from "swiper/modules";
+
 type TMainService = {
   title: string[];
   id: number;
+  images?:string[];
 };
+
 const MainServiceComp = ({ title, id }: TMainService) => {
   return (
     <>
       <h1 style={{ textAlign: "center" }}>Дознајте повеќе за нашите услуги</h1>
-      <div className={styles.OfferService}>
-        {title.map((service) => (
-          <h2 key={service}>{service}</h2>
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y, Mousewheel]}
+        spaceBetween={50}
+        slidesPerView={3}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        mousewheel={true}
+        // onSwiper={(swiper) => console.log(swiper)}
+        // onSlideChange={() => console.log("slide change")}
+        className={styles.OfferService}
+      >
+        {title.map((service, index) => (
+          <SwiperSlide key={index}>
+            <h2>{service}</h2>
+          </SwiperSlide>
         ))}
-      </div>
+      </Swiper>
     </>
   );
 };
