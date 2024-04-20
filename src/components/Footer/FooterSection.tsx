28a76003fb3b9b25d436feeea6fdbed292a5fab2
Konstantin <kvckov@yahoo.com>
2024-04-20 10:28:27 +0200

    pictures replaced with other pictures, footer component changed

--- src/components/Footer/FooterSection.tsx
+++ src/components/Footer/FooterSection.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import styles from "./styles.module.scss";
 import LocationMap from "../InfoSection/Location/LocationMap";
+import Link from "next/link";
 const FooterSection = () => {
   return (
     <div className={styles.FooterMain}>
-
       <div className={styles.imgParagraph}>
         <img src="/assets/images/some-dental-logo.png" alt="logo-img" />
       </div>
       <LocationMap />
       <div className={styles.InfoSection}>
         <div className={styles.Adress}>
+          <span>Инфо</span>
           <ul>
             <li>lorem ipsum</li>
             <li>lorem ipsum</li>
@@ -19,12 +20,23 @@ const FooterSection = () => {
           </ul>
         </div>
 
-        <div className={styles.SocialMedia}>
+        <div className={styles.Navigation}>
+          <span>Навигација</span>
           <ul>
-            <li>Facebook</li>
-            <li>Instagram</li>
-            <li>Contact us</li>
+            <Link href={"/about"}>
+              <li>За Нас</li>
+            </Link>
+            <Link href={"/ourwork"}>
+              <li>Нашата Работа</li>
+            </Link>
+            <Link href={"/services"}>
+              <li>Нашите Услуги</li>
+            </Link>
           </ul>
+        </div>
+        <div className={styles.SocialMedia}>
+          <span>Социјални медиуми</span>
+    
         </div>
       </div>
     </div>
