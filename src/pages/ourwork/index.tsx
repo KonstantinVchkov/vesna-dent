062150d9f2171e3a7b40709dfc9ac7fe90056d28
Dimitar <dimihbt@yahoo.com>
2024-09-14 14:30:45 +0200

    show more

--- src/pages/ourwork/index.tsx
+++ src/pages/ourwork/index.tsx
@@ -1,7 +1,10 @@
 import { OurWork, Work } from "@/Data/ProjectData";
 import CarouselSection from "@/components/Banner/Carousel";
-import React from "react";
+import React, { useState } from "react";
 import styles from "../../components/InfoSection/OurWork/style.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+
 export type WorkItem = {
   id: number;
   title: string;
@@ -11,9 +14,14 @@ export type TworkPage = {
   workData: typeof OurWork;
   Work: WorkItem[];
 };
+interface ExpandedState {
+  [key: number]: boolean;
+}
 const OurWorkPage = ({ workData, Work }: TworkPage) => {
+  const [expanded, setExpanded] = useState<ExpandedState>({});
+
   const generateRandomWidth = () => {
-    return Math.floor(Math.random() * (170 - 100 + 1) + 100);
+    return Math.floor(Math.random() * (100 - 100 + 1) + 50);
   };
 
   const leftRectangles = Array.from({ length: 4 }, (_, i) => (
@@ -30,17 +38,41 @@ const OurWorkPage = ({ workData, Work }: TworkPage) => {
       style={{ width: `${generateRandomWidth()}px` }}
     ></div>
   ));
+  const toggleExpand = (id: number) => {
+    setExpanded((prev) => ({
+      ...prev,
+      [id]: !prev[id],
+    }));
+  };
+
   return (
     <div>
       <div className={styles.side + " " + styles.left}>{leftRectangles}</div>
       <CarouselSection Slide={workData.slides} />
       <div className={styles.FirstPart}>
-        {Work.map((workItem) => (
-          <div key={workItem.id} className={styles.section}>
-            <h1>{workItem.title}</h1>
-            <p>{workItem.paragraph}</p>
-          </div>
-        ))}
+        {Work.map((workItem) => {
+          const paragraphClass = expanded[workItem.id]
+            ? styles.expanded
+            : styles.collapsed;
+
+          return (
+            <div key={workItem.id} className={styles.section}>
+              <h1>{workItem.title}</h1>
+              <p className={paragraphClass}>{workItem.paragraph}</p>
+              <button
+                className={`${styles.button} ${styles.hidden}`}
+                onClick={() => toggleExpand(workItem.id)}
+              >
+                {expanded[workItem.id]
+                  ? "Прочитај помалку "
+                  : "Прочитај повеќе "}
+                <FontAwesomeIcon
+                  icon={expanded[workItem.id] ? faChevronUp : faChevronDown}
+                />
+              </button>
+            </div>
+          );
+        })}
       </div>
       <div className={styles.side + " " + styles.right}>{rightRectangles}</div>
     </div>
