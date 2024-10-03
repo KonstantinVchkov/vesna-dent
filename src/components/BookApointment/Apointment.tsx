2bd816a9c49cf6a291ac76f82302fc784110e21b
Dimitar <dimihbt@yahoo.com>
2024-10-03 19:50:04 +0200

    fix BookApointment design

--- src/components/BookApointment/Apointment.tsx
+++ src/components/BookApointment/Apointment.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "./styles.module.scss";
+import Image from "next/image";
+
 export type TbookApointment = {
   handleModal: () => void;
 };
@@ -7,12 +9,41 @@ const BookApointment = ({ handleModal }: TbookApointment) => {
   return (
     <div className={styles.MainSection}>
       <div className={styles.Modal}>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ratione
-          cumque similique id natus velit, accusamus temporibus, labore maxime
-        </p>
-
-        <button onClick={handleModal}>Затвори</button>
+        {" "}
+        <div className={styles.right}>
+          <button onClick={handleModal}>X</button>
+        </div>
+        <div className={styles.center}>
+          <h1>Контакт информации</h1>
+          <div className={styles.contain}>
+            <div className={styles.center}>
+              <h3>Закажете го Вашиот термин</h3>{" "}
+              <div>
+                {" "}
+                <Image
+                  width={25}
+                  height={25}
+                  src={"/assets/images/Phone.png"}
+                  alt={"phone-img"}
+                />
+                <span>+389-78688551</span>
+              </div>
+            </div>
+            <div className={styles.center}>
+              <h3>Посететене во нашата ординација</h3>
+              <div>
+                {" "}
+                <Image
+                  width={25}
+                  height={25}
+                  src={"/assets/images/Phone.png"}
+                  alt={"phone-img"}
+                />
+                <span>Lorem ipsum dolor</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
