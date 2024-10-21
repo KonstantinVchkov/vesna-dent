2fbaf82320da1d50660c1e228f89c8e5b164eb11
Dimitar <dimihbt@yahoo.com>
2024-10-21 19:45:51 +0200

    style contact fix

--- src/components/InfoSection/Contact/ContactUs.tsx
+++ src/components/InfoSection/Contact/ContactUs.tsx
@@ -1,6 +1,8 @@
 import ButtonComp from "@/components/Button/ButtonComp";
 import React, { useState } from "react";
 import styles from "./styles.module.scss";
+import Image from "next/image";
+
 import BookApointment from "@/components/BookApointment/Apointment";
 import { info } from "./../../../Data/ProjectData";
 interface InfoItem {
@@ -17,18 +19,51 @@ interface InfoItem {
 const ContactUs: React.FC = () => {
   const [popUp, setPopUp] = useState<boolean>(false);
   const togglePopUp = () => setPopUp(!popUp);
-
   return (
     <>
       <div className={styles.center}>
         {info.map((item: InfoItem) => (
           <>
             <div key={item.id} className={styles.leftDiv}>
-              <h2>{item.title}</h2>
-              <p>{item.description}</p>
-              <p>{item.phone}</p>
-              <p>{item.workHours}</p>
-              <p>{item.email}</p>
+              <div className={styles.flex}>
+                <h2>{item.title}</h2>{" "}
+                <span className={styles.hidden}>
+                  <ButtonComp
+                    name={"Закажи термин"}
+                    handleClick={togglePopUp}
+                  />
+                </span>
+              </div>
+              <p>
+                {" "}
+                <Image
+                  width={30}
+                  height={30}
+                  src={"/assets/SocialIcons/location.png"}
+                  alt={"location-img"}
+                />{" "}
+                <span>{item.description}</span>
+              </p>
+              <p>
+                {" "}
+                <Image
+                  width={30}
+                  height={30}
+                  src={"/assets/SocialIcons/phone.png"}
+                  alt={"phone-img"}
+                />{" "}
+                <span>{item.phone}</span>
+              </p>
+              <p>
+                {" "}
+                <Image
+                  width={30}
+                  height={30}
+                  src={"/assets/SocialIcons/clock.png"}
+                  alt={"clock-img"}
+                />{" "}
+                <span>{item.workHours}</span>{" "}
+              </p>
             </div>
             <div className={styles.rightDiv}>
               <h2>{item.terminTitle}</h2>
